5850af2ef9807af8989bb65ff064525f9af5a875
Vamsi Yalamati <code4vamsi@gmail.com>
2026-09-06 01:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.68%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int josephus(int n, int k){
+
+        // Base Case
+        if(n==1)
+            return 0;
+
+        return (josephus(n-1,k)+k)%n;
+    }
+
+    int findTheWinner(int n, int k) {
+
+        return josephus(n,k)+1;
+    }
+};
